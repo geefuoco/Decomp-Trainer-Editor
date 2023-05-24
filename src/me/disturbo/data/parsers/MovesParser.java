92f17b4500ae29ce58d318119b4077f2c0fa3a18
Gianluca Fuoco <gianluca_1227@hotmail.com>
2023-05-24 18:38:47 -0400

    fix: ignore z moves

--- src/me/disturbo/data/parsers/MovesParser.java
+++ src/me/disturbo/data/parsers/MovesParser.java
@@ -9,7 +9,10 @@ public class MovesParser implements LineParser<LinkedHashMap<String, String>> {
 
     @Override
     public boolean parseLine(LinkedHashMap<String, String> moves, String line) {
-        if(line.contains("[MOVE_") && !line.contains("[MOVE_NAME_LENGTH")) {
+        if(line.contains("[MOVE_") &&
+          !line.contains("[MOVE_NAME_LENGTH") &&
+          !line.contains("FIRST_Z_MOVE")
+        ) {
             line = line.replaceAll("\\s+(?=([^\"]*\"[^\"]*\")*[^\"]*$)", "");
             String declaration = line.substring(1, line.indexOf("]"));
             String name = line.substring(line.indexOf("_(\"") + 3, line.indexOf("\")"));
