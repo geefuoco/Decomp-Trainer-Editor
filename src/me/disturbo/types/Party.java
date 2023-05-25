fece5ad801c5539f979cfedfa09df74195506c55
Gianluca Fuoco <gianluca_1227@hotmail.com>
2023-05-24 21:31:31 -0400

    fix: Use the correct party name constants instead of old way

--- src/me/disturbo/types/Party.java
+++ src/me/disturbo/types/Party.java
@@ -16,15 +16,34 @@ public class Party extends LinkedList<PartyMember> {
     public static final String extractPartyName(String name){
         try {
             return name.substring(name.indexOf("(")+1, name.length()-1);
-        } catch(ArrayIndexOutOfBoundsException e){
+        } catch(Exception e){
             System.out.println("Error when trying to extract party name: " + name);
-            e.printStackTrace();
-            return null;
+            System.out.println("WARNING: Trying to parse with old format.");
+            //If this still doesn't work, there is a error in the pokeemerald code and we should terminate here
+            return name.split("=")[1].replace("}", "");
         }
     }
 
     final String buildPartyName(){
-        return "{." + getPartyType().substring("TrainerMon".length()) + " = " + name + "}";
+        String partyType = getPartyType().substring("TrainerMon".length());
+        String result;
+        switch (partyType) {
+            case "NoItemDefaultMoves":
+                result = "NO_ITEM_DEFAULT_MOVES";
+                break;
+            case "NoItemCustomMoves":
+                result = "NO_ITEM_CUSTOM_MOVES";
+                break;
+            case "ItemDefaultMoves":
+                result = "ITEM_DEFAULT_MOVES";
+                break;
+            case "ItemCustomMoves":
+                result = "ITEM_CUSTOM_MOVES";
+                break;
+            default:
+                result = "EVERYTHING_CUSTOMIZED";
+        }
+        return result + "(" + name + ")";
     }
 
     public final String getPartyType(){
