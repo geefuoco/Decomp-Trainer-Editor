72cc1b0c5573c617f9049919e33d939dbd575a44
Gianluca Fuoco <gianluca_1227@hotmail.com>
2023-05-28 15:06:12 -0400

    feat: panel for showing pokemon sprite

---
+++ app/src/main/java/me/disturbo/ui/party/PartyMemberPicPanel.java
@@ -0,0 +1,47 @@
+package me.disturbo.ui.party;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.CropImageFilter;
+import java.awt.image.FilteredImageSource;
+import java.io.File;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import me.disturbo.main.MainActivity;
+
+public class PartyMemberPicPanel extends JPanel {
+
+    private ImageIcon icon;
+    private JLabel label;
+
+    public PartyMemberPicPanel() {
+        setLayout(new GridLayout(2, 1));
+        setBackground(Color.WHITE);
+
+        icon = new ImageIcon();
+        label = new JLabel("Sprite:");
+        label.setHorizontalTextPosition(JLabel.LEFT);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setIcon(icon);
+        add(label);
+    }
+
+    public final void setImage(String path){
+        icon = new ImageIcon(MainActivity.projectDirectory.getPath() +
+            File.separator +
+            path);
+
+        Image image = icon.getImage();
+        image = createImage(new FilteredImageSource(
+                image.getSource(),
+                new CropImageFilter(0, 0, 64, 64)));
+
+        icon.setImage(image);
+        label.setIcon(icon);
+    }
+
+}
