4a43fc47fe71532e02c9eae4203a0c9c25ba84c1
Gianluca Fuoco <gianluca_1227@hotmail.com>
2023-05-28 21:01:51 -0400

    fix: filter out certain words

--- app/src/main/java/me/disturbo/data/parsers/NatureParser.java
+++ app/src/main/java/me/disturbo/data/parsers/NatureParser.java
@@ -7,6 +7,9 @@ import java.util.LinkedList;
 public class NatureParser implements LineParser<LinkedList<String>> {
     @Override
     public boolean parseLine(LinkedList<String> natures, String line) {
+        if(line.contains("STATS") || line.contains("EVO")){
+            return true;
+        }
         if(line.contains("NATURE_")) {
             natures.add(line.split(" ")[1]);
         }
