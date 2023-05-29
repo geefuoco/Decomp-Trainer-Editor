f337c9f7a5e9e3032a0c9071764166071a56e446
Gianluca Fuoco <gianluca_1227@hotmail.com>
2023-05-28 22:24:33 -0400

    fix: remove unused import

--- app/src/test/java/me/disturbo/data/parsers/NatureParserTest.java
+++ app/src/test/java/me/disturbo/data/parsers/NatureParserTest.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 
 import org.junit.jupiter.api.Test;
